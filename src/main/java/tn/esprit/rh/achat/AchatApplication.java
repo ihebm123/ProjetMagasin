0a235b4f55e1967f851ad48117f0b2b88d838c5b
Iheb MHAMDI <iheb.mhamdi@esprit.tn>
2022-10-20 18:36:54 +0100

    until nexus commit

--- src/main/java/tn/esprit/rh/achat/AchatApplication.java
+++ src/main/java/tn/esprit/rh/achat/AchatApplication.java
@@ -3,7 +3,12 @@ package tn.esprit.rh.achat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableWebMvc
+@EnableSwagger2
 @EnableScheduling
 @SpringBootApplication
 public class AchatApplication {
---
+++ src/test/java/tn/esprit/rh/achat/AchatApplicationTests.java
@@ -0,0 +1,32 @@
+package tn.esprit.rh.achat;
+
+
+
+
+
+
+
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+
+
+//@RunWith(SpringRunner.class)
+@SpringBootTest(classes = AchatApplication.class)
+class AchatApplicationTests {
+	
+	
+	@Test
+	void contextLoads() {
+		
+		
+	}
+
+
+	
+
+}
