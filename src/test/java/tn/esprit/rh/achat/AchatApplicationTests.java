a8063a48657a3cf096a796f7781725b1f9e1c19d
Abdelkader.ferchichi <abdelkader.ferchichi@esprit.tn>
2022-11-03 14:49:45 +0100

    testUnit

--- src/test/java/tn/esprit/rh/achat/AchatApplicationTests.java
+++ src/test/java/tn/esprit/rh/achat/AchatApplicationTests.java
@@ -1,24 +1,12 @@
 package tn.esprit.rh.achat;
 
 
-
-
-
-
-
-
-
 import org.junit.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-
-
-
-
-//@RunWith(SpringRunner.class)
-@SpringBootTest(classes = AchatApplication.class)
+@SpringBootTest
 class AchatApplicationTests {
-	
 	
 	@Test
 	void contextLoads() {
